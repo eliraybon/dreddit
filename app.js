e9b1aa023251e7cc61a54f4d6f5e179c65c7a2db
eliraybon <elr0615@gmail.com>
2019-11-25 15:18:35 -0500

    Add user model

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+
+const User = require('./models/User');
+
 const app = express();
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
@@ -10,5 +14,16 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+  // debugger;   
+  const user = new User({
+    username: 'testUser',
+    password: 'password'
+  })
+  user.save()
+  res.send("Hello World");
+});
+
+app.use('/api/users', users);
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
