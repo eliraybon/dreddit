638d83eceba78ebe823dbc82fdccccd2d47ba3a4
eliraybon <elr0615@gmail.com>
2019-11-25 13:51:44 -0500

    Setup MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require('mongoose');
+
 const app = express();
+const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
