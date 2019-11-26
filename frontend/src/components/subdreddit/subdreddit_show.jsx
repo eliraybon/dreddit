5691adaa35e6abe7ae983936f083a89a1f3710b3
Seth Ullman <seth.ullman98@gmail.com>
2019-11-26 10:06:37 -0700

    add subdreddit_show container and component

---
+++ frontend/src/components/subdreddit/subdreddit_show.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SubDredditShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>I am a subDreddit!</h1>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/components/subdreddit/subdreddit_show_container.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import SubDredditShow from './subdreddit_show';
+import { fetchSubDreddit } from '../../actions/sub_dreddit_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    sub: state.entities.sub[ownProps.match.params.subId]
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchSubDreddit: subId => dispatch(fetchSubDreddit(subId))
+  };
+};
+
+export default connnect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SubDredditShow);
