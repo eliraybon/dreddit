6197fc95801fa5f07d96bf2063e365c21481d049
eliraybon <elr0615@gmail.com>
2019-11-29 16:21:02 -0500

    Add subdreddit links to user show

--- frontend/src/components/subdreddit/subdreddit_index_item.jsx
+++ frontend/src/components/subdreddit/subdreddit_index_item.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class SubDredditIndexItem extends React.Component {
   render() {
     const { sub } = this.props;
     return (
       <li className="subdreddit-index-item">
-        <p>{sub.title}</p>
+        <Link to={`/subdreddits/${sub._id}`}>
+          {sub.title}
+        </Link>
       </li>
     )
   }
