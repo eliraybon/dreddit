36700e81fcb2f27787e10b5ed8f8c1415d60b15b
Seth Ullman <seth.ullman98@gmail.com>
2019-12-02 11:53:53 -0700

    add post item vote styling

--- frontend/src/components/post/post_index_item.jsx
+++ frontend/src/components/post/post_index_item.jsx
@@ -99,33 +99,43 @@ export default class PostIndexItem extends React.Component {
     const { post } = this.props;
     return (
       <li className="pii">  
-        <Link to={`/posts/${post._id}`}>
-          {post.title}  
-        </Link>
+        
+        
+          <div className='pii-votes'>
+            <button onClick={this.upvote} className='pii-upvote'></button>
+            {this.state.votes}
+            <button onClick={this.downvote} className='pii-downvote'></button>
+          </div>
+          <Link to={`/posts/${post._id}`} className='pii-show-link'>
+            <div className='pii-top'>
+              
+              {post.title} 
+            </div>
+            <div className='pii-media'>
+              {post.imgUrl && (
+              <img
+                src={post.imgUrl}
+                width="200px"
+                height="200px"
+              />
+              )}
 
-        {post.imgUrl && (
-          <img
-            src={post.imgUrl}
-            width="200px"
-            height="200px"
-          />
-        )}
-
-        {post.videoUrl && (
-          <video src={post.videoUrl}
-            width="320px"
-            height="240px"
-            controls
-          >
-          </video>
-        )}
+              {post.videoUrl && (
+                <video src={post.videoUrl}
+                  width="320px"
+                  height="240px"
+                  controls
+                >
+                </video>
+              )} 
+            </div>
+          </Link>
 
         {/* {this.renderTest()} */}
 
-        {this.state.votes}
-        <button onClick={this.upvote}>Upvote</button>
-        <button onClick={this.downvote}>Downvote</button>
-        {this.renderDeleteButton()}
+        <div className='pii-bottom-nav'>
+          {this.renderDeleteButton()}
+        </div>
       </li>
     )
   }
