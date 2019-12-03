9df6d335f56fc4a2c464e72921814763e5985098
eliraybon <elr0615@gmail.com>
2019-12-02 20:54:52 -0500

    Quick test

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -6,9 +6,10 @@ import SubdredditIndex from '../subdreddit/subdreddit_index_container';
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { userSubsDropdown: false };
+    this.state = { userSubsDropdown: false, searchBarOpen: false };
 
     this.container = React.createRef();
+    this.searchContainer = React.createRef();
 
     this.logoutUser = this.logoutUser.bind(this);
     this.getLinks = this.getLinks.bind(this);
