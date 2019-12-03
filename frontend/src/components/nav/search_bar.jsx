82831fbbdedac7683627dd5d3f975d1abd2c5dbc
eliraybon <elr0615@gmail.com>
2019-12-02 21:21:39 -0500

    Fix search bar dropdown

--- frontend/src/components/nav/search_bar.jsx
+++ frontend/src/components/nav/search_bar.jsx
@@ -4,20 +4,52 @@ import SubDredditIndex from '../subdreddit/subdreddit_index_container';
 export default class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { subs: [] }
+    this.state = { subs: [], resultsDropdown: false }
+
+    this.container = React.createRef();
 
     this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleOutsideClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleOutsideClick);
   }
 
   handleUpdate(e) {
     e.preventDefault();
     this.props.updateSearchTerm(e.currentTarget.value)
       .then(res => {
-        this.setState({ subs: res.subDreddits })
+        this.setState({ subs: res.subDreddits, resultsDropdown: true })
       })
   }
 
+  handleOutsideClick = e => {
+    if (!this.container.current.contains(e.target)) {
+
+      this.setState({ resultsDropdown: false });
+    }
+  }
+
+  handleOutsideClick(e) {
+    if (this.container.current && !this.container.current.contains(e.target)) {
+      this.setState({ userSubsDropdown: false });
+    };
+  }
+
+  toggleResultsDropdown = () => {
+    if (!this.state.subs.length) return null;
+
+    this.setState(state => {
+      return { userSubsDropdown: !state.userSubsDropdown }
+    });
+  }
+
   render() {
+
     return (
       <div>
         <input
@@ -25,10 +57,14 @@ export default class SearchBar extends React.Component {
           type="text"
           placeholder="Search Dreddit"
           onChange={this.handleUpdate}
+          // ref={this.container}
+          onClick={this.toggleResultsDropdown}
         />
 
-        <div className="search-results">
-          <SubDredditIndex subs={ this.state.subs } />
+        <div className="search-results" ref={this.container}>
+          {this.state.resultsDropdown && (
+            <SubDredditIndex subs={ this.state.subs } />
+          )}
         </div>
       </div>
     )
