893b71361c91253f41877ba790ec0d241057f4e5
Seth Ullman <seth.ullman98@gmail.com>
2020-04-22 20:26:09 -0600

    updates keys

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dbUser:nR0z2r0Tjc5jrbI6@cluster0-00jog.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: '^!9dTi]+(`oi1Hj',
+  awsAccessKey: 'AKIAJS2OMQ63JKVXCZ6A',
+  awsSecretAccessKey: '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'
+}
